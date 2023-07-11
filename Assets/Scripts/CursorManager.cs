6613c7c854ce682f69dcb5fc04965a3012696ece
riboojo <ricardo.bqz@gmail.com>
2023-07-10 23:01:59 -0600

    Add selectable choices
    Remove logic to select choices by mouse clicking
    Add logic to select choices with numpad

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -11,24 +11,39 @@ public class CursorManager : MonoBehaviour
     [SerializeField]
     private Transform[] delimiters; // x:9_19 y:-3.69_6.29
 
-    public float speed = 1f;
-
-    private CursorLockMode lockMode;
+    private float speed = 2f;
 
     void Awake()
     {
-        //lockMode = CursorLockMode.Locked;
-        //Cursor.lockState = lockMode;
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
     }
 
     void Update()
+    {
+        MoveCursor();
+    }
+
+    public bool IsCursorOnScreen()
+    {
+        Vector3 view = screen.ScreenToViewportPoint(Input.mousePosition);
+        bool isOutside = view.x < 0 || view.x > 1 || view.y < 0 || view.y > 1;
+
+        return isOutside;
+    }
+
+    public Vector3 GetCursorPosition()
+    {
+        return cursor.transform.position;
+    }
+
+    void MoveCursor()
     {
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, speed);
         Vector3 newCursorPos = new Vector3(screen.ScreenToWorldPoint(mousePos).x, screen.ScreenToWorldPoint(mousePos).y, cursor.transform.position.z);
 
         Vector3 curCursorPor = cursor.transform.position;
-        
+
         if ((newCursorPos.x >= delimiters[0].position.x) && (newCursorPos.x <= delimiters[1].position.x))
         {
             cursor.transform.position = new Vector3(newCursorPos.x, cursor.transform.position.y, cursor.transform.position.z);
@@ -38,10 +53,5 @@ public class CursorManager : MonoBehaviour
         {
             cursor.transform.position = new Vector3(cursor.transform.position.x, newCursorPos.y, cursor.transform.position.z);
         }
-    }
-
-    public Vector3 GetCursorPosition()
-    {
-        return cursor.transform.position;
     }
 }
